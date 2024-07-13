4e00345ee7159fc5fcdc0394c376c8ddec1b3185
Suraj Prajapati <surajrp108@outlook.com>
2024-07-13 23:00:04 +0530

    Ribbon added

---
+++ SmartTech-Addin/Ribbon/SmartTachAiRibbon.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Office.Core;
+using Microsoft.Office.Interop.Outlook;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Office = Microsoft.Office.Core;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SmartTech_Addin
+{
+    [ComVisible(true)]
+    public class SmartTachAiRibbon : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+        private static readonly HttpClient client = new HttpClient();
+
+        public SmartTachAiRibbon()
+        {
+        }
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+            return GetResourceText("SmartTech_Addin.Ribbon.SmartTechAiRibbon.xml");
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+
+        public void onClickDraftBtn(IRibbonControl control)
+        {
+            DraftReplyAllAsync();
+        }
+
+        public void onClickSummarizeBtn(IRibbonControl control)
+        {
+            Globals.ThisAddIn.TaskPane.Visible = true;
+        }
+
+        public void onClickRepharse(IRibbonControl control)
+        {
+            DraftReplyWithAttachment();
+        }
+
+        public bool isEmailSelected() 
+        {
+            var application = Globals.ThisAddIn.SelectedEmail;
+            bool email = application != null;
+            return email;
+        }
+
+        private async Task DraftReplyAllAsync()
+        {
+            string url = "https://google.com/";
+            //string response = await GetApiDataAsync(url);
+            //Console.WriteLine(response);
+
+            var mailItem = Globals.ThisAddIn.SelectedEmail as MailItem;
+            if (mailItem != null)
+            {
+                var replyAll = mailItem.ReplyAll();
+                replyAll.HTMLBody = "hello" + "\n" + replyAll.HTMLBody;
+                replyAll.Display();
+            }
+        }
+
+        private void DraftReplyWithAttachment()
+        {
+            var mailItem = Globals.ThisAddIn.SelectedEmail as MailItem;
+            if (mailItem != null)
+            {
+                var reply = mailItem.Reply();
+                reply.Body = "Hello";
+                reply.Attachments.Add(mailItem, OlAttachmentType.olEmbeddeditem);
+                reply.Display();
+            }
+        }
+
+
+        async Task<string> GetApiDataAsync(string url)
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode(); // Throw an exception if the status code is not 2xx
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+            return responseBody;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ SmartTech-Addin/ThisAddIn.cs
@@ -0,0 +1,98 @@
+﻿using Outlook = Microsoft.Office.Interop.Outlook;
+using Microsoft.Office.Interop.Outlook;
+using Microsoft.Office.Tools;
+using System;
+
+namespace SmartTech_Addin
+{
+    public partial class ThisAddIn
+    {
+        private CustomTaskPane myCustomTaskPane;
+        private Outlook.Inspectors inspectors;
+        private Outlook.MailItem selectedEmail;
+
+        public CustomTaskPane TaskPane
+        {
+            get
+            {
+                return myCustomTaskPane;
+            }
+        }
+        public Outlook.MailItem SelectedEmail
+        {
+            get
+            {
+                return selectedEmail;
+            }
+        }
+
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            return new SmartTachAiRibbon();
+        }
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            Outlook.Explorer explorer = this.Application.ActiveExplorer();
+            explorer.SelectionChange += new Outlook.ExplorerEvents_10_SelectionChangeEventHandler(Explorer_SelectionChange);
+
+            /*
+              myCustomTaskPane.Visible = true;
+
+             inspectors = this.Application.Inspectors;
+             inspectors.NewInspector +=
+             new Microsoft.Office.Interop.Outlook.InspectorsEvents_NewInspectorEventHandler(Inspectors_NewInspector);
+             */
+        }
+
+        private void Explorer_SelectionChange()
+        {
+            Outlook.Explorer explorer = this.Application.ActiveExplorer();
+            Outlook.Selection selection = explorer.Selection;
+
+            if (selection.Count > 0)
+            {
+                Object selectedItem = selection[1]; // Get the first selected item
+
+                if (selectedItem is Outlook.MailItem)
+                {
+                    selectedEmail = (Outlook.MailItem)selectedItem;
+                }
+            }
+        }
+
+        private void Inspectors_NewInspector(Inspector Inspector)
+        {
+            Outlook.MailItem mailItem = Inspector.CurrentItem as Outlook.MailItem;
+            if (mailItem != null)
+            {
+                if (mailItem.EntryID == null)
+                {
+                    mailItem.Subject = "This text was added by using code";
+                    mailItem.Body = "This text was added by using code";
+                }
+
+            }
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            // Note: Outlook no longer raises this event. If you have code that 
+            //    must run when Outlook shuts down, see https://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
